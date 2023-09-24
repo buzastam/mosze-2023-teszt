20033738571e4ff6a5b43b8405b33b64f3a3f967
buzastam <50901647+buzastam@users.noreply.github.com>
2023-09-24 16:29:08 +0200

    javítva

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
-//ez nem hiba de ide beírnám a: using namespace std -t a könnyebb olvashatóság miatt
+using namespace std;
 
-constexpr int N_ELEMENTS = 100; // ha száz elemű a tömb akor 0-99 ig írja a számokat és nem kapjuk meg a 100*2=200 at
+constexpr int N_ELEMENTS = 101;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // nem pontos a név
-    std::cout << '1-100 ertekek duplazasa' // macska köröm kell a cout parancshoz, és hiányzik a pontos veszző
-    for (int i = 0;)  // nincs feltétel és nincs megadva, hogy mennyivel nőljön a ciklus
+    int *b = new int[N_ELEMENTS];
+    cout << "1-100 ertekek duplazasa" << endl;
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        b[i] = i * 2;
+        b[i] = i*2;
     }
-    for (int i = 0; i; i++) //hiányzik a feltétel, és ha ki akarjuk írni a 100 nak a 2 szeresét akkor egytől indul a ciklus
+
+    for (int i = 1; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // nincs pontosvessző és én kiírnám utána az értékét b[i] majd endl -al elválasztanám az olvashatóság miatt
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //kezdőérték kell az átlagnak
-    for (int i = 0; i < N_ELEMENTS, i++) // nem veszző az ELEMENTS után hanem pontos veszző
-    {
-        atlag += b[i] //potnosveszző hiánya
+        cout << "Ertek: " << b[i] << endl; 
     }
-    atlag /= N_ELEMENTS;  
-    std::cout << "Atlag: " << atlag << std::endl;
+
+    cout << "Atlag szamitasa: " << endl;
+    int atlag = 0; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
+    {
+        atlag += b[i]; 
+    }
+    atlag /= N_ELEMENTS; 
+    cout << "Atlag: " << atlag << endl;
+
     return 0;
 }
