f12a73c148ec58c405758c205eb42ce8c5bf26e5
buzastam <50901647+buzastam@users.noreply.github.com>
2023-09-24 16:32:58 +0200

    összevont

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
+using namespace std;
 
-constexpr int N_ELEMENTS = 100;
+constexpr int N_ELEMENTS = 101;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];
+    cout << "1-100 ertekek duplazasa" << endl;
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        b[i] = i * 2;
+        b[i] = i*2;
     }
-    for (int i = 0; i; i++)
+
+    for (int i = 1; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i]
+        cout << "Ertek: " << b[i] << endl; 
     }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
+
+    cout << "Atlag szamitasa: " << endl;
+    int atlag = 0; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
+    {
+        atlag += b[i]; 
+    }
+    atlag /= N_ELEMENTS; 
+    cout << "Atlag: " << atlag << endl;
+
     return 0;
 }
